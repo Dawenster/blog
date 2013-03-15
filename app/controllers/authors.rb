31a4e8f25c8f93f5c29c46e6eb6088d8442aa15f
Dawenster <dstwen@gmail.com>
2013-03-15 00:00:33 -0700

    Created logout and create page links in navigation

--- app/controllers/authors.rb
+++ app/controllers/authors.rb
@@ -1,22 +1,25 @@
 get '/create/author' do
-
 	erb :create_author
 end
 
 post '/new/author' do
-	author = Author.create(params[:author])
-	puts author.id
-	puts session.inspect
-	session[:current_user_id] = author.id
-	puts session[:current_user_id]
+	new_author = Author.create(params[:author])
+	session[:current_user_id] = new_author.id
 end
 
 get '/author/login' do
 	current_user = Author.authenticate(params[:login])
 	if current_user
 		session[:current_user_id] = current_user.id
+		@posts = Post.all
 		erb :index
 	else
 		erb :create_author
 	end
+end
+
+get '/author/logout' do
+	session.delete(:current_user_id)
+	@posts = Post.all
+	erb :index
 end
