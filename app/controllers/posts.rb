b2affa0d35ec4452dcc1db4ecbcf92233cc7cc71
Dawenster <dstwen@gmail.com>
2013-03-14 23:30:15 -0700

    Created single post page and index post views

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -7,3 +7,9 @@ post '/new/post' do
 	params[:post][:author_id] = session[:current_user_id]
 	Post.create(params[:post])
 end
+
+get '/post/:id' do
+	@post = Post.find(params[:id])
+	@author_name = Author.find(@post.author_id).name
+	erb :single_post
+end
